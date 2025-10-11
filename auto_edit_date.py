09e9f2541daf3ece314f5d895965d7c5639077bc
Nhan Truong <nhantruong1298@gmail.com>
2025-10-11 18:15:14 +0700

    auto edit day

---
+++ auto_edit_date.py
@@ -0,0 +1,154 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from typing import Dict, Any
+from openpyxl import load_workbook
+
+
+def login(driver, username_value, password_value):
+    """Login to the website with provided credentials"""
+    username = driver.find_element(By.NAME, "username")
+    username.send_keys(username_value)
+
+    password = driver.find_element(By.NAME, "password")
+    password.send_keys(password_value)
+
+    driver.find_element(By.XPATH, "//button[text()='Đăng nhập']").click()
+    time.sleep(1)
+
+
+def select_date(driver, plan, date):
+    """Sửa ngày format MM/DD/YYYY"""
+    driver.find_element(By.NAME, f"plan[{plan}]").click()
+    time.sleep(0.5)
+    driver.find_element(
+        By.CSS_SELECTOR, f"td[data-day='{date.replace('\"', '')}']"
+    ).click()
+    time.sleep(0.5)
+    driver.find_element(By.NAME, f"plan[{plan}]").click()
+    time.sleep(0.5)
+
+
+def replace_date(driver, plan, dateFromExcel):
+    """Sửa ngày format MM/DD/YYYY"""
+    element = driver.find_element(By.NAME, f"plan[{plan}]")
+    time.sleep(0.5)
+    dateFromWeb = element.get_attribute("value")
+    dateParts = dateFromWeb.split(" ")
+
+    element.clear()
+    time.sleep(0.5)
+    element.send_keys(dateFromExcel + " " + dateParts[1])
+
+
+def save_date(driver):
+    """Lưu ngày đã chọn"""
+
+    button_save = driver.find_element(
+        By.XPATH,
+        "//button[@type='button' and contains(@class, 'btn-success') and contains(@onclick, 'savePlan')]",
+    )
+
+    driver.execute_script("arguments[0].scrollIntoView(true);", button_save)
+    time.sleep(0.5)
+    # button_save.click()
+    time.sleep(0.5)
+
+
+def select_note_and_fill_and_save(driver, name):
+    """Chọn note khác và điền nội dung và lưu"""
+    element_to_click = driver.find_element(
+        By.XPATH, "//a[@class='chosen-single' and contains(., 'Chọn loại Note')]"
+    )
+    driver.execute_script("arguments[0].scrollIntoView(true);", element_to_click)
+    time.sleep(0.5)
+    element_to_click.click()
+    time.sleep(0.5)
+    element_to_click = driver.find_element(
+        By.XPATH, "//li[contains(@class, 'active-result') and text()='Note khác']"
+    )
+    element_to_click.click()
+    time.sleep(0.5)
+    driver.find_element(By.ID, "note_comment").send_keys(
+        f"{name} - Chỉnh Ngày - Cắt Hình"
+    )
+    time.sleep(0.5)
+    # save_button = driver.find_element(
+    #     By.XPATH,
+    #     "//button[@id='uploadSnap' and @type='button' and contains(@onclick, 'uploadSnap')]",
+    # )
+
+    # driver.execute_script("arguments[0].scrollIntoView(true);", save_button)
+    # save_button.click()
+    time.sleep(0.5)
+
+
+def read_excel_to_map_with_hyperlinks(file_path: str) -> Dict[str, Any]:
+    """
+    Đọc dữ liệu từ file Excel, lấy URL ẩn từ hyperlink ở cột A
+    và String hiển thị ở cột B, lưu vào dictionary.
+    """
+    data_map = {}
+
+    try:
+        workbook = load_workbook(file_path, data_only=False)
+        sheet = workbook.active  
+
+        for row in sheet.iter_rows(min_row=1):
+            url_cell = row[0].value  
+            string_cell = row[1].value  
+
+            if url_cell and isinstance(url_cell, str):
+                if url_cell.startswith('=HYPERLINK("') and url_cell.endswith('")'):
+                    url_cell = url_cell[12:-2]  
+
+            url_parts = url_cell.split(",")
+            if url_parts[0].strip().startswith("https://") and "-" in string_cell:
+                data_map[url_parts[0].replace('"', "").strip()] = string_cell.replace(
+                    '"', ""
+                ).strip()
+            else:
+                continue
+
+    except FileNotFoundError:
+        print(f"Lỗi: Không tìm thấy file tại đường dẫn: {file_path}")
+    except Exception as e:
+        print(f"Lỗi xảy ra khi đọc file Excel: {e}")
+
+    return data_map
+
+
+# Code execution starts here
+def runAutoEditDate():
+    data = read_excel_to_map_with_hyperlinks("/Users/nhan.tt/Desktop/Book1.xlsx")
+
+    driver = webdriver.Chrome()
+
+    for url, dateFromExcel in data.items():
+        driver.get(url)
+
+        try:
+            driver.find_element(By.NAME, "username")
+            driver.find_element(By.NAME, "password")
+
+            login(driver, "HCM03723", "cpm3711")
+            time.sleep(2)
+
+            driver.get(url)
+            print("Đã login")
+        except:
+            print("Khong cần login")
+            pass
+
+        replace_date(driver, "time_checkin", dateFromExcel)
+        replace_date(driver, "time_checkout", dateFromExcel)
+        replace_date(driver, "time_upload", dateFromExcel)
+
+        time.sleep(2)
+
+        # save_date(driver)
+        select_note_and_fill_and_save(driver, "Tên")
+        time.sleep(1)
+
+    time.sleep(5)
+    driver.quit()
---
+++ main.py
@@ -0,0 +1,3 @@
+from auto_edit_date import runAutoEditDate
+
+runAutoEditDate()
